a66d898c0815439b07d6eb1790567cfca3a8569f
DeathSaverMS1 <manvikg23@gmail.com>
2024-12-03 22:09:49 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://metallic.eu.org/", nil
+            return "https://house.shirokuriwaki.com/", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
